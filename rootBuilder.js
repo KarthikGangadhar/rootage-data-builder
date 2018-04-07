97d0218f941aa5e641cfefe7a7b9907a28686c9a
karthik <karthikg1643@gmail.com>
2018-04-07 22:31:13 +0530

    added synchronous data update logic

--- rootBuilder.js
+++ rootBuilder.js
@@ -1,32 +1,80 @@
 'use strict';
 
+const config = require('./utils/config');
 const fs = require('fs');
 const path = require('path');
 const utils = require('./utils/utils');
+const es = require('event-stream');
+const ArrayStream = require('arraystream');
 
-// utils.readDirectory()
+// let filePath = path.join(__dirname, 'data', 'movie');
+// let moviesUrl = `${config.ROOTAGE_BASE_URL}${config.ROOATAGE_MOVIE_URL}`;
+// utils.readDirectory(filePath).then((content) => {
+//     console.log(content);
+//     let stream = ArrayStream.create(content);
+//     let contentaraay = [];
+//     let csv_row_count = 0;
 
-const readAndWriteFile = (dir) => {
-    return new Promise((resolve, reject) => {
-        console.log(dir)
-        let writeContent = JSON.parse(fs.readFileSync(dir).toString());
-        // let reduced_content = ;
-        resolve(writeContent.reduce((content, data) => {
-            data['type'] = 'top-rated-movies';
-            return [...content, data];
-        }, []));
-    }).catch((err) => {
-        console.log({
-            msg: err.message,
-            stack: err.stack
+//     stream.on('data', (value, key) => {
+//         stream.pause();
+//         utils.validate_response(value).then((validData) => {
+//             if(!validData.id){
+//                 validData['id'] = key;
+//             }
+//             utils.postResponse(moviesUrl, validData).then(response => {
+//                 console.log(JSON.stringify(response));
+//                 stream.resume();
+//             });
+//         }).catch(err => {
+//             console.log(err);
+//         });
+//         // console.log('value :', value); // hoge, fuga, piyo
+//         console.log('key :', key); // 0,    1,    2
+//         ++csv_row_count
+//         console.log(`array count: ${csv_row_count} `);
+//     });
+
+//     stream.on('end', () => { // emitted at the end of iteration
+//         console.log('end');
+//     });
+
+//     stream.on('error', (e) => { // emitted when an error occurred
+//         console.log(e);
+//     });
+// });
+
+let filePath = path.join(__dirname, 'data', 'prize');
+let moviesUrl = `${config.ROOTAGE_BASE_URL}${config.ROOATAGE_PRIZE_URL}`;
+utils.readDirectory(filePath).then((content) => {
+    console.log(content);
+    let stream = ArrayStream.create(content);
+    let contentaraay = [];
+    let csv_row_count = 0;
+
+    stream.on('data', (value, key) => {
+        stream.pause();
+        utils.validate_response(value).then((validData) => {
+            // if(!validData.id){
+            //     validData['id'] = key;
+            // }
+            utils.postResponse(moviesUrl, validData).then(response => {
+                console.log(JSON.stringify(response));
+                stream.resume();
+            });
+        }).catch(err => {
+            console.log(err);
         });
+        // console.log('value :', value); // hoge, fuga, piyo
+        console.log('key :', key); // 0,    1,    2
+        ++csv_row_count
+        console.log(`array count: ${csv_row_count} `);
     });
-}
 
-let filePath = path.join(__dirname, 'data', 'movie', 'top-rated-movies-01.json')
-readAndWriteFile(filePath).then(content => {
-    fs.writeFile(filePath, JSON.stringify(content), (err) => {
-        if (err) return console.log(err);
-        console.log("The file was saved!");
+    stream.on('end', () => { // emitted at the end of iteration
+        console.log('end');
     });
-})
+
+    stream.on('error', (e) => { // emitted when an error occurred
+        console.log(e);
+    });
+});
--- utils/utils.js
+++ utils/utils.js
@@ -8,7 +8,12 @@ const ReadDirectory = async dir =>
     fs.readdirSync(dir).reduce((files, file) => {
         const name = path.join(dir, file);
         const isDirectory = fs.statSync(name).isDirectory();
-        return isDirectory ? [...files, ...getAllFiles(name)] : [...files, require(name)];
+        if (isDirectory) {
+            ReadDirectory(name)
+        } else {
+            files = files.concat(require(name));
+            return files;
+        }
     }, []);
 
 const PostResponse = (url, body) => {
@@ -22,7 +27,67 @@ const PostResponse = (url, body) => {
     });
 };
 
+const ReadAndWriteFile = (dir) => {
+    return new Promise((resolve, reject) => {
+        console.log(dir)
+        let writeContent = JSON.parse(fs.readFileSync(dir).toString());
+        resolve(writeContent.reduce((content, data) => {
+            data['type'] = 'top-rated-movies';
+            return [...content, data];
+        }, []));
+    }).then(content => {
+        fs.writeFile(filePath, JSON.stringify(content), (err) => {
+            if (err) return console.log(err);
+            console.log("The file was saved!");
+        });
+    }).catch((err) => {
+        console.log({
+            msg: err.message,
+            stack: err.stack
+        });
+    });
+};
+
+const Validate_response = (response) => {
+    return new Promise((resolve, reject) => {
+        let data = response
+        if (response && Object.keys(response).length > 0) {
+            format_object(response).then((obj) => {
+                resolve(obj);
+            })
+        } else {
+            reject("response ids not valid");
+        }
+    })
+}
+
+const format_object = (playerResponse) => {
+    return new Promise((resolve, reject) => {
+        if (playerResponse) {
+            for (let key in playerResponse) {
+                if (key === "v" || key === "ttl") {
+                    delete playerResponse[key]
+                } else if (playerResponse[key] && typeof playerResponse[key] === "object") {
+                    format_object(playerResponse[key])
+                } else if (playerResponse[key] && typeof playerResponse[key] === "array" && playerResponse[key].length === 0) {
+                    delete playerResponse[key];
+                } else if (playerResponse[key] && typeof playerResponse[key] === "array" && playerResponse[key].length !== 0) {
+                    format_object(playerResponse[key])
+                } else if (playerResponse[key] === "" || playerResponse[key] === null || playerResponse[key] === undefined) {
+                    delete playerResponse[key];
+                }
+            }
+            resolve(playerResponse)
+        } else {
+            reject("RESPONSE IS EMPTY");
+        }
+
+    });
+}
+
 module.exports = {
+    readAndWriteFile: ReadAndWriteFile,
     readDirectory: ReadDirectory,
-    postResponse: PostResponse
+    postResponse: PostResponse,
+    validate_response: Validate_response
 };
